07cddf0c4ab8ac962b75945fb2849b8cb3cbd69e
Mokee.nmy <ningmuyu@live.com>
2020-10-16 17:50:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# git clone https://github.com/P3TERX/xxx package/xxx
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean
+
