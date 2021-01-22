3dfdf7b73af8d2303f1d2f62baa8cda930a4cd1c
Mokee.nmy <ningmuyu@live.com>
2021-01-22 16:02:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-package' feeds.conf.default
 
