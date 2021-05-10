d27d9a9becc9173cd665299c7526ba3206b6fc37
Mokee.nmy <ningmuyu@live.com>
2021-05-10 09:56:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-
+sed -i '$a src-git ddnsto https://github.com/linkease/nas-packages.git' feeds.conf.default
